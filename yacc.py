c77160f16e108cf287a332f9fd6f038d6fef6ade
VitalyVV <v.volobuev@innopolis.ru>
2018-10-14 21:22:53 +0300

    Some work done

--- yacc.py
+++ yacc.py
@@ -37,27 +37,27 @@ def p_statement(p):
 
 def p_global_const_defn(p):
     """
-    global-const-defn  :  C_GLOBAL C_CONST var-decl t_SEMICOLON
+    global-const-defn  :  C_GLOBAL C_CONST var-decl SEMICOLON
     """
     p[0] = ('GLOBAL_CONSTANT', p[3])
 
 def p_import_stmt(p):
     """
-    import-stmt  :  D_IMPORT module-path t_SEMICOLON
-                 | D_IMPORT t_STR_LITERAL t_SEMICOLON
+    import-stmt  :  D_IMPORT module-path SEMICOLON
+                 | D_IMPORT STR_LITERAL SEMICOLON
     """
     p[0] = ('IMPORT', p[2])
 
 def p_module_path(p):
     """
-    module-path  :  t_ID path-star
+    module-path  :  ID path-star
     """
     p[0] = ('MODULE_PATH', tuple(p[1]))
 
 def p_path_star(p):
     """
     path-star   :
-                | t_DOT t_ID path-star
+                | DOT ID path-star
     """
     if p[1] != '':
         p[0] = ('PATH', p[2], tuple(p[3]))
@@ -66,7 +66,7 @@ def p_path_star(p):
 
 def p_pragma_stms(p):
     """
-    pragma-stmt  :  C_PRAGMA t_ID expr  t_SEMICOLON
+    pragma-stmt  :  C_PRAGMA ID expr  SEMICOLON
     """
     p[0] = ('PRAGMA', p[2], p[3])
 
@@ -90,7 +90,7 @@ def p_func_hdr(p):
 def p_type_params(p):
     """
     type-params  :
-                 | t_LESS var-name comma-name-star t_GREATER
+                 | LESS var-name comma-name-star GREATER
     """
     if p[1] != '':
         p[0] = ('NAME', p[2], tuple(p[3]))
@@ -100,7 +100,7 @@ def p_type_params(p):
 def p_comma_name_star(p):
     """
     comma-name-star :
-                    | t_COMMA var-name comma-name-star
+                    | COMMA var-name comma-name-star
     """
     if p[1] != '':
         p[0] = ('NAME', p[2], tuple(p[3]))
@@ -110,7 +110,7 @@ def p_comma_name_star(p):
 def p_formal_arg_list(p):
     """
     formal-arg-list  :
-                     | t_LPAREN ((formal-arg comma-args-star) | )  t_RPAREN
+                     | LPAREN ((formal-arg comma-args-star) | )  RPAREN
     """
     if p[1] != '':
         p[0] = ('ARGS', tuple(p[2]))
@@ -120,7 +120,7 @@ def p_formal_arg_list(p):
 def p_comma_args_star(p):
     """
     comma-args-star :
-                    |    t_COMMA formal-arg comma-args-star
+                    |    COMMA formal-arg comma-args-star
     """
     if p[1] != '':
         p[0] = ('ARGS', p[2], tuple(p[3]))
@@ -129,7 +129,7 @@ def p_comma_args_star(p):
 
 def p_formal_arg(p):
     """
-    formal-arg  :  type-prefix (  | t_RANGE) var-name type-suffix ( | (t_ASSIGN expr))
+    formal-arg  :  type-prefix (  | RANGE) var-name type-suffix ( | (ASSIGN expr))
     """
     range_ = ''
     assign = ''
@@ -139,7 +139,7 @@ def p_formal_arg(p):
 
 def p_swift_func_defn(p):
     """
-    swift-func-defn  :  annotation-star func-hdr t_ARROW block
+    swift-func-defn  :  annotation-star func-hdr ARROW block
     """
     p[0] = (p[2], tuple(p[1]), p[4])
     
@@ -155,14 +155,14 @@ def p_annotation_star(p):
 
 def p_app_func_defn(p):
     """
-    app-func-defn  :  annotation-star C_APP func-hdr t_LBRACE app-body t_RBRACE
+    app-func-defn  :  annotation-star C_APP func-hdr LBRACE app-body RBRACE
     """
     p[0] = ('APP', p[3], p[5])
 
 
 def p_app_body(p):
     """
-    app-body  :  app-arg-expr app-arg-expr-star app-out-star ( | t_SEMICOLON)
+    app-body  :  app-arg-expr app-arg-expr-star app-out-star ( | SEMICOLON)
     """
     p[0] = ('BODY', p[1], tuple(p[2]), tuple(p[3]), p[4])
 
@@ -170,7 +170,7 @@ def p_app_body(p):
 def p_app_out_star(p):
     """
     app-out-star    :
-                    | (E_STDIN | E_STDOUT | E_STDERR) t_ASSIGN expr app-out-star
+                    | (E_STDIN | E_STDOUT | E_STDERR) ASSIGN expr app-out-star
     """
     if p[1] != '':
         p[0] = ('APP_OUT', (p[2], p[1], p[3]), tuple(p[4]))
@@ -196,7 +196,7 @@ def p_foreign_func_defn(p):
 
 def p_foreign_func_body(p):
     """
-    foreign-func-body  :  t_STR_LITERAL t_STR_LITERAL ( | t_STR_LITERAL) ( | (t_LBRACKET (t_STR_LITERAL | t_MUL_STR_LITERAL) t_RBRACKET))
+    foreign-func-body  :  STR_LITERAL STR_LITERAL ( | STR_LITERAL) ( | (LBRACKET (STR_LITERAL | MUL_STR_LITERAL) RBRACKET))
     """
     p[0] = ('FOREIGN_FUNC', p[1], p[2], p[3], p[4])
 
@@ -209,7 +209,7 @@ def p_var_decl(p):
 def p_var_decl_rest_star(p):
     """
     var-decl-rest-star  :
-                        | t_COMMA var-decl-rest var-decl-rest-star
+                        | COMMA var-decl-rest var-decl-rest-star
     """
     if p[1] != '':
         p[0] = ('VAR_DECL', p[2], tuple(p[3]))
@@ -218,44 +218,56 @@ def p_var_decl_rest_star(p):
 
 def p_var_decl_rest(p):
     """
-    var-decl-rest  :  var-name type-suffix ( | var-mapping) ( | (t_ASSIGN expr))
+    var-decl-rest  :  var-name type-suffix ( | var-mapping) ( | (ASSIGN expr))
     """
+    p[0] = ('VAR_DECL_TAILER', p[1], p[2], p[3], p[4])
 
 def p_type_prefix(p):
     """
     type-prefix  :  type-name
-                    | param-type
+                 | param-type
     """
+    p[0] = p[1]
 
 def p_param_type(p):
     """
-    param-type  :  type-name t_LESS standalone-type t_GREATER
+    param-type  :  type-name LESS standalone-type GREATER
     """
+    p[0] = (p[3], p[1])
 
 def p_type_suffix(p):
     """
-    type-suffix  :  (t_LBRACKET ( | standalone-type) t_RBRACKET)*
+    type-suffix  :
+                 | (LBRACKET ( | standalone-type) RBRACKET) type_suffix
     """
+    if p[1] != '':
+        p[0] = ('TYPE_SUFFIX', p[2], p[1], tuple(p[3]))
+    else:
+        p[0] = p[1]
 
 def p_standalone_type(p):
     """
     standalone-type  :  type-prefix type-suffix
     """
+    p[0] = ('STANDALONE_TYPE', p[1], p[2])
 
 def p_var_mapping(p):
     """
-    var-mapping  :  t_LESS expr t_GREATER
+    var-mapping  :  LESS expr GREATER
     """
+    p[0] = ('VAR_MAPPING', p[1])
 
 def p_block(p):
     """
-    block  :  t_LBRACE statement * t_RBRACE
+    block  :  LBRACE statement-star RBRACE
     """
+    p[0] = ('CODE_BLOCK', tuple(p[2]))
 
 def p_stmt_chain(p):
     """
-    stmt-chain  :  chainable-stmt (t_SEMICOLON | t_ARROW statement)
+    stmt-chain  :  chainable-stmt ((SEMICOLON | ARROW) statement)
     """
+    p[0] = ('STATEMENT_CHAIN', ('STATEMENT', p[2]), p[1])
 
 def p_chainable_stmt(p):
     """
@@ -264,82 +276,99 @@ def p_chainable_stmt(p):
                       | var-decl
                       | assignment
     """
+    p[0] = ('CHAINABLE_STMT', p[1])
 
 def p_assignment(p):
     """
-    assignment  :  (lval-list | t_LPAREN lval-list t_RPAREN) (t_ASSIGN | t_PLUS_AS) expr-list
+    assignment  :  (lval-list | LPAREN lval-list RPAREN) (ASSIGN | PLUS_AS) expr-list
     """
+    p[0] = ('ASSIGNMENT', p[3], p[1], p[2])
 
 def p_update_stmt(p):
     """
-    update-stmt  :  var-name t_LESS t_ID t_GREATER t_UPD expr t_SEMICOLON
+    update-stmt  :  var-name LESS ID GREATER UPD expr SEMICOLON
     """
+    p[0] = ('UPDATE', p[1], p[6])
 
 def p_if_stmt(p):
     """
-    if-stmt  :  IF t_LPAREN expr t_RPAREN block ( | (ELSE block))
+    if-stmt  :  IF LPAREN expr RPAREN block ( | (ELSE block))
     """
+    p[0] = ('IF', p[3], p[4], (p[5]))
 
 def p_switch_stmt(p):
     """
-    switch-stmt  :  S_SWITCH t_LPAREN expr t_RPAREN t_LBRACE case * ( | default) t_RBRACE
+    switch-stmt  :  S_SWITCH LPAREN expr RPAREN LBRACE case-star( | default) RBRACE
     """
+    p[0] = ('SWITCH', p[3], tuple(p[6]), p[7])
+
+def p_case_star(p):
+    """
+    case-star   :
+                |   case case-star
+    """
+    if p[1] != '':
+        p[0] = ('CASES', p[1], tuple(p[2]))
+    else:
+        p[0] = p[1]
 
 def p_case(p):
     """
-    case  :  S_CASE int-literal t_COLON statement*
+    case  :  S_CASE int-literal COLON statement-star
     """
+    p[0] = (p[1], p[2], tuple(p[4]))
 
 def p_default(p):
     """
-    default  :  S_DEFAULT t_COLON statement*
+    default  :  S_DEFAULT COLON statement-star
     """
+    p[0] = (p[1], tuple(p[3]))
 
 def p_wait_stmt(p):
     """
-    wait-stmt  :  E_WAIT ( | E_DEEP) t_LPAREN expr-list t_RPAREN block
+    wait-stmt  :  E_WAIT ( | E_DEEP) LPAREN expr-list RPAREN block
     """
 
 def p_foreach_loop(p):
     """
-    foreach-loop  :  annotation-star S_FOREACH var-name ( | (t_COMMA var-name)) S_IN expr block
+    foreach-loop  :  annotation-star S_FOREACH var-name ( | (COMMA var-name)) S_IN expr block
     """
 
 def p_for_loop(p):
     """
-    for-loop  :  annotation-star S_FOR t_LPAREN for-init-list t_SEMICOLON expr ; for-update-list t_RPAREN block
+    for-loop  :  annotation-star S_FOR LPAREN for-init-list SEMICOLON expr ; for-update-list RPAREN block
     """
 
 def p_while_loop(p):
     """
-    while-loop  :   annotation WHILE t_LPAREN expr t_RPAREN block
+    while-loop  :   annotation WHILE LPAREN expr RPAREN block
     """
 
 def p_for_init_list(p):
     """
-    for-init-list  :  for-init (t_COMMAfor-init)*
+    for-init-list  :  for-init (COMMAfor-init)*
     """
 
 def p_for_init(p):
     """
     for-init  :  for-assignment
-              | type-prefix var-name type-suffix t_ASSIGN expr
+              | type-prefix var-name type-suffix ASSIGN expr
     """
 
 def p_for_update_list(p):
     """
-    for-update-list  :  for-assignment (t_COMMA for-assignment)*
+    for-update-list  :  for-assignment (COMMA for-assignment)*
     """
 
 def p_for_assignment(p):
     """
-    for-assignment  :  var-name t_ASSIGN expr
+    for-assignment  :  var-name ASSIGN expr
     """
     p[0] = (p[2], p[1], p[3])
 
 def p_iterate_loop(p):
     """
-    iterate-loop  :  S_ITERATE var-name block S_UNTIL t_LPAREN expr t_RPAREN
+    iterate-loop  :  S_ITERATE var-name block S_UNTIL LPAREN expr RPAREN
     """
     p[0] = (p[1], p[2], p[3], p[4], p[6])
 
@@ -352,7 +381,7 @@ def p_expr(p):
 def p_or_expr(p):
     """
     or-expr  :  and-expr
-             | or-expr t_LOG_OR and-expr
+             | or-expr LOG_OR and-expr
     """
     if len(p) == 4:
         p[0] = (p[2], p[1], p[3])
@@ -362,7 +391,7 @@ def p_or_expr(p):
 def p_and_expr(p):
     """
     and-expr  :  eq-expr
-              | and-expr t_LOG_AND eq-expr
+              | and-expr LOG_AND eq-expr
     """
     if len(p) == 4:
         p[0] = (p[2], p[1], p[3])
@@ -372,7 +401,7 @@ def p_and_expr(p):
 def p_eq_expr(p):
     """
     eq-expr  :  cmp-expr
-             | eq-expr (t_EQUAL | t_NOT_EQUAL) eq-expr
+             | eq-expr (EQUAL | NOT_EQUAL) eq-expr
     """
     if len(p) == 4:
         p[0] = (p[2], p[1], p[3])
@@ -383,7 +412,7 @@ def p_eq_expr(p):
 def p_cmp_expr(p):
     """
     cmp-expr  :  add-expr
-              | cmp-expr (t_LESS | t_ASSIGN | t_GREATER | t_GREATER_EQ) add-expr
+              | cmp-expr (LESS | ASSIGN | GREATER | GREATER_EQ) add-expr
     """
     if len(p) == 4:
         p[0] = (p[2], p[1], p[3])
@@ -393,7 +422,7 @@ def p_cmp_expr(p):
 def p_add_expr(p):
     """
     add-expr  :  mult-expr
-              | add-expr (t_PLUS | t_MINUS) mult-expr
+              | add-expr (PLUS | MINUS) mult-expr
     """
     if len(p) == 4:
         p[0] = (p[2], p[1], p[3])
@@ -403,7 +432,7 @@ def p_add_expr(p):
 def p_mult_expr(p):
     """
     mult-expr  :  unary-expr
-               | mult-expr (t_MULT | t_DIV | t_MULTPER | t_DOUBLEPER | t_MOD) unary-expr
+               | mult-expr (MULT | DIV | MULTPER | DOUBLEPER | MOD) unary-expr
     """
     if len(p) == 4:
         p[0] = (p[2], p[1], p[3])
@@ -413,7 +442,7 @@ def p_mult_expr(p):
 def p_unary_expr(p):
     """
     unary-expr  :  postfix-expr
-                  | (t_MINUS | t_EXCLAMATION) postfix-expr
+                  | (MINUS | EXCLAMATION) postfix-expr
     """
     if len(p) == 3:
         p[0] = (p[1], p[2])
@@ -432,13 +461,13 @@ def p_postfix_expr(p):
 
 def p_array_subscript(p):
     """
-    array-subscript  :  t_LBRACKET expr t_RBRACKET
+    array-subscript  :  LBRACKET expr RBRACKET
     """
     p[0] = p[2]
 
 def p_struct_subscript(p):
     """
-    struct-subscript  :  t_DOT t_ID
+    struct-subscript  :  DOT ID
     """
     p[0] = (p[1], p[2])
 
@@ -447,7 +476,7 @@ def p_base_expr(p):
     base-expr  :  literal
                  | func-call
                  | var-name
-                 | t_LPAREN expr t_RPAREN
+                 | LPAREN expr RPAREN
                  | tuple-constructor
                  | array-constructor
     """
@@ -458,17 +487,17 @@ def p_base_expr(p):
 
 def p_func_call(p):
     """
-    func-call  :  annotation* func-name t_LPAREN func-call-arg-list t_RPAREN
+    func-call  :  annotation* func-name LPAREN func-call-arg-list RPAREN
     """
 
 def p_func_call_arg_list(p):
     """
-    func-call-arg-list  :  (expr | kw-expr) (t_COMMA (expr | kw-expr))*
+    func-call-arg-list  :  (expr | kw-expr) (COMMA (expr | kw-expr))*
     """
 
 def p_tuple_constructor(p):
     """
-    tuple-constructor  :  t_LPAREN expr t_COMMA expr (t_COMMA expr) * t_RPAREN
+    tuple-constructor  :  LPAREN expr COMMA expr (COMMA expr) * RPAREN
     """
 
 def p_array_constructor(p):
@@ -481,41 +510,41 @@ def p_array_constructor(p):
 
 def p_array_list_constructor(p):
     """
-    array-list-constructor  :  t_LBRACKET expr-list? t_RBRACKET
+    array-list-constructor  :  LBRACKET expr-list? RBRACKET
     """
 
 def p_array_range_constructor(p):
     """
-    array-range-constructor  :  t_LBRACKET expr t_COLON expr (t_COLON expr)? t_RBRACKET
+    array-range-constructor  :  LBRACKET expr COLON expr (COLON expr)? RBRACKET
     """
 
 def p_array_kv_constructor(p):
     """
-    array-kv-constructor  :  t_LBRACE (array-kv-elem (t_COMMA array-kv-elem)*)? t_RBRACE
+    array-kv-constructor  :  LBRACE (array-kv-elem (COMMA array-kv-elem)*)? RBRACE
     """
 
 def p_array_kv_elem(p):
     """
-    array-kv-elem  :  expr t_COLON expr
+    array-kv-elem  :  expr COLON expr
     """
     p[0] = (p[2], p[1], p[3])
 
 def p_annotation(p):
     """
-    annotation  :  t_AT t_ID | t_AT kw-expr
+    annotation  :  AT ID | AT kw-expr
     """
     p[0] = (p[1], p[2])
 
 def p_kw_expr(p):
     """
-    kw-expr  :  t_ID t_ASSIGN expr
+    kw-expr  :  ID ASSIGN expr
     """
     p[0] = (p[2], p[1], p[3])
 
 def p_literal(p):
     """
-    literal  :  t_STR_LITERAL
-             | t_MUL_STR_LITERAL 
+    literal  :  STR_LITERAL
+             | MUL_STR_LITERAL
              | int-literal
              | float-literal
              | bool-literal
@@ -524,10 +553,10 @@ def p_literal(p):
 
 def p_float_literal(p):
     """
-    float-literal  :  t_DOUBLE
-                     | t_DOUBLE
-                     | t_INF
-                     | t_NAN
+    float-literal  :  DOUBLE
+                     | DOUBLE
+                     | INF
+                     | NAN
     """
     p[0] = p[1]
 
@@ -540,7 +569,7 @@ def  p_bool_literal(p):
 
 def p_expr_list(p):
     """
-    expr-list  :  expr (t_COMMA expr)*
+    expr-list  :  expr (COMMA expr)*
     """
 
 def p_type_name(p):
@@ -561,25 +590,25 @@ def p_type_name(p):
 
 def p_const_name(p):
     """
-    const-name  :   D_LET t_ID
+    const-name  :   D_LET ID
     """
     p[0] = (p[1], p[2])
 
 def p_var_name(p):
     """
-    var-name  :     t_ID
+    var-name  :     ID
     """
     p[0] = (p[1], p[2])
 
 def p_func_name(p):
     """
-    func-name  :  D_FUNCTION t_ID
+    func-name  :  D_FUNCTION ID
     """
     p[0] = (p[1], p[2])
 
 def p_lval_list(p):
     """
-    lval-list  :  lval-expr (t_COMMA lval-expr)*
+    lval-list  :  lval-expr (COMMA lval-expr)*
     """
 
 def p_lval_expr(p):
@@ -589,10 +618,10 @@ def p_lval_expr(p):
 
 def p_app_arg_expr(p):
     """
-    app-arg-expr  :  ( | t_AT) var-name
+    app-arg-expr  :  ( | AT) var-name
                     | literal
                     | array-constructor
-                    | t_LPAREN expr t_RPAREN
+                    | LPAREN expr RPAREN
     """
     if len(p) == 3:
         p[0] = ('APP_ARG', p[2], p[1])
